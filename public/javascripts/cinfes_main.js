551949944a9e83f4fba314a3dbf79c64f26d1dc3
Lonzia Shay <lonziashay@Lonzias-MacBook-Air.local>
2012-10-20 17:58:57 -0700

    Make poster demensions fixed

--- public/javascripts/cinfes_main.js
+++ public/javascripts/cinfes_main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   $('.modal.show-on-domready').modal();
+
+    $('.flip').click(function(){
+        $(this).find('.card').addClass('flipped').mouseleave(function(){
+            $(this).removeClass('flipped');
+        });
+        return false;
+    });
+	
+	$('.front img').attr({'width': '600px', 'height': '938px' })
 });
