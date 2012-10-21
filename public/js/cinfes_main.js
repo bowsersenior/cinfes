e7504b7fbd63a54f1c524bfa5b1f83d28d2d5e28
Mani Tadayon <bowsersenior@gmail.com>
2012-10-20 23:51:00 -0700

    Add pusher+toastr

--- public/js/cinfes_main.js
+++ public/js/cinfes_main.js
@@ -1,7 +1,24 @@
+if (typeof window.CINFES === 'undefined') {
+  window.CINFES = {}
+}
+
 $(document).ready(function(){
   $('.modal.show-on-domready').modal();
 
   $('.flip').click(function(){
       $(this).find('.card').toggleClass('flipped');
   });
+
+
+  CINFES.socket = new Pusher('1368c91dfcb03dac81b3');
+  CINFES.movie_channel = CINFES.socket.subscribe('movies');
+
+  CINFES.movie_channel.bind('notification', function(data) {
+    if (data.type === 'success') {
+      toastr.success(data.message);
+    } else {
+      toastr.info(data.message);
+    }
+  });
+
 });
